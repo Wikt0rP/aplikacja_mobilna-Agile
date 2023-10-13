7300f4b859bc0d62466eae6ee99dfdb19c820050
BuildTools <unconfigured@null.spigotmc.org>
2023-10-13 16:53:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int pinClean = 0;
+
+    String userName = "Unknown";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView welcomeText = (TextView) findViewById(R.id.WelcomeText);
+        welcomeText.setText("Witaj " + userName);
+        pinClean = 0;
+    }
+
+    public void PinClick(View view)
+    {
+
+    }
+}
