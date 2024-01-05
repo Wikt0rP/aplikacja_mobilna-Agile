4110c6374ad945ad95e75f905c7cb7ff5c54fecf
ASzazX <prokopiszynwiktor@gmail.com>
2024-01-05 01:47:23 +0100

    Implementing API

--- app/src/main/java/com/example/myapplication/APIUser.java
+++ app/src/main/java/com/example/myapplication/APIUser.java
@@ -70,9 +70,69 @@ public class APIUser
 
 
     }
+    public void getTokens()
+    {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseURL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
+        ApiService apiService = retrofit.create(ApiService.class);
+        Call<ResponseBody> call = apiService.createToken(userName, password);
+
+        call.enqueue(new Callback<ResponseBody>()
+        {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response)
+            {
+                if (response.isSuccessful())
+                {
+                    try
+                    {
+                        JSONObject json = new JSONObject(response.body().string());
+                        accessToken = json.getString("access");
+                        refreshToken = json.getString("refresh");
+
+                    } catch (JSONException | IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else {
+                    Log.d("APIUser", "Error: " + response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t)
+            {
+                Log.d("APIUser", "Failure: " + t.getMessage());
+            }
+        });
+
+
+    }
     public String getAccessToken()
     {
-        return this.accessToken;
+       if (this.accessToken != null)
+       {
+           return this.accessToken;
+       }
+       else
+       {
+           getTokens();
+           return this.accessToken;
+       }
+    }
+    public String getRefreshToken()
+    {
+        if (this.refreshToken != null)
+        {
+            return this.refreshToken;
+        }
+        else
+        {
+            getTokens();
+            return this.refreshToken;
+        }
     }
 }
