aea3fe3a678d23082e03cf8ec292f9502574c364
ASzazX <prokopiszynwiktor@gmail.com>
2024-01-07 19:01:49 +0100

    UI changes

--- app/src/main/java/com/example/myapplication/RemoveExpenseActivity.java
+++ app/src/main/java/com/example/myapplication/RemoveExpenseActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -63,19 +64,26 @@ public class RemoveExpenseActivity extends AppCompatActivity {
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
 
                 Expense expenseToRemove = expenseList.get(position);
-                expenseList.remove(position);
+
 
                 APIExpense apiExpense = new APIExpense(getIntent().getStringExtra("accessToken"), getIntent().getStringExtra("refreshToken"));
                 Log.d("API Expense", "Rozpoczynam usuwanie wydatku o id: " + id);
                 apiExpense.deleteExpense(new DeleteExpenseCallback() {
                     @Override
-                    public void onSuccess() {
+                    public void onSuccess()
+                    {
                         Log.d("API Expense", "Successfully deleted expense");
-
+                        expenseList.remove(position);
+                        Intent intent = new Intent(RemoveExpenseActivity.this, AfterLogin.class);
+                        intent.putExtra("accessToken", getIntent().getStringExtra("accessToken"));
+                        intent.putExtra("refreshToken", getIntent().getStringExtra("refreshToken"));
+                        startActivity(intent);
+                        Log.d("AfterLogin", "Anuluj");
                     }
 
                     @Override
-                    public void onError(String errorMessage) {
+                    public void onError(String errorMessage)
+                    {
                         Log.d("API Expense", "Failed to delete expense: " + errorMessage);
                     }
                 }, expenseToRemove.getId());
