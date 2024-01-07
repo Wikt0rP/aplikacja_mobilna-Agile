206c65f2b99c5834a10682eaf2290d0fa2ea0147
ASzazX <prokopiszynwiktor@gmail.com>
2024-01-07 01:31:27 +0100

    Color list

--- app/src/main/java/com/example/myapplication/AfterLogin.java
+++ app/src/main/java/com/example/myapplication/AfterLogin.java
@@ -136,12 +136,12 @@ public class AfterLogin extends AppCompatActivity
         {
             switch (which) {
                 case 0:
-                    Intent intent = new Intent(this, addMoney.class);
+                    Intent intent = new Intent(AfterLogin.this, addMoney.class);
                     startActivity(intent);
                     Log.d("AfterLogin", "Dodaj/usuń pieniądze");
                     break;
                 case 1:
-                    Intent intent2 = new Intent(this, addExpense.class);
+                    Intent intent2 = new Intent(AfterLogin.this, addExpense.class);
                     startActivity(intent2);
                     Log.d("AfterLogin", "Wydatek");
                     break;
