522980a90c824fba6cff2e76af89b878fab52615
ASzazX <prokopiszynwiktor@gmail.com>
2024-01-07 18:49:12 +0100

    UI changes

--- app/src/main/java/com/example/myapplication/ApiService.java
+++ app/src/main/java/com/example/myapplication/ApiService.java
@@ -36,9 +36,8 @@ public interface ApiService
     @POST("/expense/create/")
     Call <ResponseBody> addExpense(@Header("Authorization") String AccessToken, @Field("kwota") double amount, @Field("tytul") String title);
 
-    @FormUrlEncoded
     @DELETE("/expense/delete")
-    Call <ResponseBody> deleteExpense(@Header("Authorization") String AccessToken, @Field("id") int id);
+    Call <ResponseBody> deleteExpense(@Header("Authorization") String AccessToken, @Header("id") int id);
 
 
 
