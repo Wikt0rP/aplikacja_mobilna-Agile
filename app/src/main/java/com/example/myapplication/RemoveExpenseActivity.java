dbe38b391467b90b3c10d5814a1e4b7d8187cb89
ASzazX <prokopiszynwiktor@gmail.com>
2024-01-07 19:13:33 +0100

    UI changes

--- app/src/main/java/com/example/myapplication/RemoveExpenseActivity.java
+++ app/src/main/java/com/example/myapplication/RemoveExpenseActivity.java
@@ -1,5 +1,7 @@
 package com.example.myapplication;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -64,30 +66,37 @@ public class RemoveExpenseActivity extends AppCompatActivity {
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
 
                 Expense expenseToRemove = expenseList.get(position);
+                new AlertDialog.Builder(RemoveExpenseActivity.this)
+                        .setTitle("Usuń wydatek")
+                        .setMessage("Czy na pewno chcesz usunąć ten wydatek?")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener()
+                        {
+                            public void onClick(DialogInterface dialog, int which) {
 
+                                APIExpense apiExpense = new APIExpense(getIntent().getStringExtra("accessToken"), getIntent().getStringExtra("refreshToken"));
+                                Log.d("API Expense", "Rozpoczynam usuwanie wydatku o id: " + id);
+                                apiExpense.deleteExpense(new DeleteExpenseCallback() {
+                                    @Override
+                                    public void onSuccess() {
+                                        Log.d("API Expense", "Successfully deleted expense");
+                                        expenseList.remove(position);
+                                        Intent intent = new Intent(RemoveExpenseActivity.this, AfterLogin.class);
+                                        intent.putExtra("accessToken", getIntent().getStringExtra("accessToken"));
+                                        intent.putExtra("refreshToken", getIntent().getStringExtra("refreshToken"));
+                                        startActivity(intent);
+                                        Log.d("AfterLogin", "Anuluj");
+                                    }
 
-                APIExpense apiExpense = new APIExpense(getIntent().getStringExtra("accessToken"), getIntent().getStringExtra("refreshToken"));
-                Log.d("API Expense", "Rozpoczynam usuwanie wydatku o id: " + id);
-                apiExpense.deleteExpense(new DeleteExpenseCallback() {
-                    @Override
-                    public void onSuccess()
-                    {
-                        Log.d("API Expense", "Successfully deleted expense");
-                        expenseList.remove(position);
-                        Intent intent = new Intent(RemoveExpenseActivity.this, AfterLogin.class);
-                        intent.putExtra("accessToken", getIntent().getStringExtra("accessToken"));
-                        intent.putExtra("refreshToken", getIntent().getStringExtra("refreshToken"));
-                        startActivity(intent);
-                        Log.d("AfterLogin", "Anuluj");
-                    }
-
-                    @Override
-                    public void onError(String errorMessage)
-                    {
-                        Log.d("API Expense", "Failed to delete expense: " + errorMessage);
-                    }
-                }, expenseToRemove.getId());
-                Log.d("API Expense", "Zakończono usuwanie wydatku o id: " + id);
+                                    @Override
+                                    public void onError(String errorMessage) {
+                                        Log.d("API Expense", "Failed to delete expense: " + errorMessage);
+                                    }
+                                }, expenseToRemove.getId());
+                                Log.d("API Expense", "Zakończono usuwanie wydatku o id: " + id);
+                            }
+                        }).setNegativeButton(android.R.string.no, null)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
 
                 return true;
             }
